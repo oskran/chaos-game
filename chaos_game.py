75032a49636aad2b51b3e011ac1bed120acc6c84
Oskar Randen <oskar.randen@gmail.com>
2019-11-17 21:56:39 +0100

    Iterating

--- chaos_game.py
+++ chaos_game.py
@@ -23,19 +23,20 @@ class ChaosGame:
         self.r = r
 
         self._generate_ngon()
+        self.st_point = self._starting_point()
 
     def _generate_ngon(self):
         """Generates the ngon for a given number of sides
         """
         theta = np.linspace(0, (np.pi * 2), self.n + 1)
 
-        c = []
+        c = np.zeros(shape=(self.n, 2))
 
         for i in range(self.n):
             c1 = np.sin(theta[i])
             c2 = np.cos(theta[i])
 
-            c.append([c1, c2])
+            c[i] = [c1, c2]
 
         self.corners = c
 
@@ -56,19 +57,39 @@ class ChaosGame:
         # Divide all weigths by the sum of the weigths so that they sum to one
         w = w / np.sum(w)
 
-        point = [0, 0]
+        point = np.zeros(2)
 
         for i, c in enumerate(self.corners):
-            print(i, c)
-
             point[0] = point[0] + c[0] * w[i]
-
             point[1] = point[1] + c[1] * w[i]
 
         return point
 
+    def iterate(self, steps, discard=5):
+        random_corner = np.random.randint(self.n)
 
-game = ChaosGame(5, 0.5)
+        midway_point = (
+            self.r * self.st_point + (1 - self.r) * self.corners[random_corner]
+        )
+
+        points = np.zeros(shape=(steps, 2))
+        points[0] = midway_point
+
+        for i in range(1, steps):
+            random_corner = np.random.randint(self.n)
+
+            points[i] = (
+                self.r * points[i - 1] + (1 - self.r) * self.corners[random_corner]
+            )
+
+        return points[discard:]
 
 
-print(game._starting_point())
+game = ChaosGame(3, 0.5)
+
+points = game.iterate(1000)
+
+plt.scatter(*zip(*points))
+
+plt.axis("equal")
+plt.show()
