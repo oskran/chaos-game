43b52233fa4cd7608c19f1df78fdadc319d4f034
Oskar Randen <oskar.randen@gmail.com>
2019-11-20 21:20:26 +0100

    Reorganized as class, and added documentation

--- fern.py
+++ fern.py
@@ -3,6 +3,30 @@ import matplotlib.pyplot as plt
 
 
 class AffineTransform:
+    """Affine transformation function
+
+    Takes a pair of coordinates, transforms them, and returns them.
+
+    Parameters
+    ----------
+    a : float
+        Parameter of the transformation function
+    b : float
+        Parameter of the transformation function
+    c : float
+        Parameter of the transformation function
+    d : float
+        Parameter of the transformation function
+    e : float
+        Parameter of the transformation function
+    f : float
+        Parameter of the transformation function
+    Returns
+    -------
+    ndarray of float
+        The transformed coordinates
+    """
+
     def __init__(self, a, b, c, d, e, f):
         self.a = a
         self.b = b
@@ -17,33 +41,76 @@ class AffineTransform:
         ) + np.array([self.e, self.f])
 
 
-func1 = AffineTransform(0, 0, 0, 0.16, 0, 0)
-func2 = AffineTransform(0.85, 0.04, -0.04, 0.85, 0, 1.60)
-func3 = AffineTransform(0.20, -0.26, 0.23, 0.22, 0, 1.60)
-func4 = AffineTransform(-0.15, 0.28, 0.26, 0.24, 0, 0.44)
+class Fern:
+    """Uses AffineTransform to create Barnsley Fern
+    
+    Returns
+    -------
+    [type]
+        [description]
+    """
 
-functions = [func1, func2, func3, func4]
+    def __init__(self):
+        func1 = AffineTransform(0, 0, 0, 0.16, 0, 0)
+        func2 = AffineTransform(0.85, 0.04, -0.04, 0.85, 0, 1.60)
+        func3 = AffineTransform(0.20, -0.26, 0.23, 0.22, 0, 1.60)
+        func4 = AffineTransform(-0.15, 0.28, 0.26, 0.24, 0, 0.44)
+        self.functions = [func1, func2, func3, func4]
 
-prob = [0.01, 0.85, 0.07, 0.07]
-prob_cumulative = np.cumsum(prob)
+        probabilities = [0.01, 0.85, 0.07, 0.07]
+        self.prob_cumulative = np.cumsum(probabilities)
+
+    def choose_function(self):
+        """Chooses a transformation function at random
+
+        Chooses based on a list of probabilities.
+        
+        Returns
+        -------
+        AffineTransform
+            A randomly choosen transformation function
+        """
+        r = np.random.random()
+        for j, p in enumerate(self.prob_cumulative):
+            if r < p:
+                return self.functions[j]
+
+    def iterate(self, n):
+        """Generates points iteratively by using affine transformations
+        
+        Parameters
+        ----------
+        n : int
+            Number of iterations
+        """
+        self.n = n
+
+        points = np.zeros(shape=(self.n, 2))
+        points[0] = [0, 0]
+
+        for i in range(1, self.n):
+            func = self.choose_function()
+            points[i] = func(points[i - 1][0], points[i - 1][1])
+
+        self.points = points
+
+    def plot(self, s=1, c="green"):
+        """Plots the fern
+        
+        Parameters
+        ----------
+        s : int, optional
+            Size of the points, by default 1
+        c : str, optional
+            Color of the fern, by default "green"
+        """
+        plt.scatter(*zip(*self.points), s=s, c=c)
+        plt.axis("equal")
+        plt.savefig("barnsley_fern.png", dpi=300)
+        plt.show()
 
 
-def choose_function():
-    r = np.random.random()
-    for j, p in enumerate(prob_cumulative):
-        if r < p:
-            return functions[j]
-
-
-n = 50000
-
-points = np.zeros(shape=(n, 2))
-points[0] = [0, 0]
-
-for i in range(1, n):
-    func = choose_function()
-    points[i] = func(points[i - 1][0], points[i - 1][1])
-
-plt.scatter(*zip(*points), s=1, c="green")
-plt.axis("equal")
-plt.show()
+if __name__ == "__main__":
+    fern = Fern()
+    fern.iterate(50000)
+    fern.plot(s=0.1)
